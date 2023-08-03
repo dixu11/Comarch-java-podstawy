d6851e35a8398a025f1b034d984cd80c495adc6d
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-08-03 10:17:25 +0200

    program demo Koty

---
+++ src/main/java/FirstProgram.java
@@ -0,0 +1,14 @@
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
+
+/*
+ Witajcie
+
+ Co słychać?
+* */
--- src/main/java/Koty.java
+++ src/main/java/Koty.java
@@ -20,5 +20,6 @@ public class Koty {
         } else {
             System.out.println("Potrzebujesz " + iloscKuwet + " kuwety");
         }
+
     }
 }
