d0cefc2c61d4ee6e43bd3b53b24fb7c9ee64f439
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-08-03 10:24:23 +0200

    wyświetlanie demo

--- src/main/java/FirstProgram.java
+++ src/main/java/FirstProgram.java
@@ -3,6 +3,16 @@ public class FirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println();
+        System.out.print("Co słychać?");
+        System.out.print("...");
+        System.out.println("test");
+
+        //Specjalne znaki:
+        // /"
+        // /n
+        // //
+        // /t
     }
 
 }
