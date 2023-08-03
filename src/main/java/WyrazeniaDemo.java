7ee24d8165ba8677cf1a3f1f4ed61195e1854b13
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-08-03 11:02:34 +0200

    wyrazenia wyjaśnienie

---
+++ src/main/java/WyrazeniaDemo.java
@@ -0,0 +1,14 @@
+public class WyrazeniaDemo {
+    public static void main(String[] args) {
+        System.out.println("hello"); //String - napis
+        System.out.println(10); //int - liczba całkowita
+        System.out.println(10 + 10);
+        System.out.println("hello" + 10);
+        System.out.println("10abc" + 10);
+        System.out.println(6.5 / 4);  //double  - liczba dziesiętna
+        System.out.println(true); //boolean - typ logiczny (true/false)
+        System.out.println(false);
+        System.out.println(10 > 5);
+        System.out.println('a'); //char
+    }
+}
