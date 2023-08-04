82bd889d5f288d483a41a2dbfa70707cad9c3532
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-08-04 09:49:56 +0200

    modulo

---
+++ src/main/java/dzienDrugi/Modulo.java
@@ -0,0 +1,26 @@
+package dzienDrugi;
+
+public class Modulo {
+
+    public static void main(String[] args) {
+        //reszta z dzielenia - pomaga np sprawdzić czy liczba jest parzysta x % 2 == 0
+        System.out.println(1%3);
+        System.out.println(2%3);
+        System.out.println(3%3);
+        System.out.println(4%3);
+        System.out.println(5%3);
+        System.out.println(6%3);
+
+
+      /*  System.out.println(5%2);
+        System.out.println(5%3);
+        System.out.println(5%4);
+        System.out.println(5%5);
+        System.out.println(5%6);
+        System.out.println(5%7);
+        System.out.println(5%8);
+        System.out.println(5%9);
+        System.out.println(5%10);
+        System.out.println(5%11);*/
+    }
+}
